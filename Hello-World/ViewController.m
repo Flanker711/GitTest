a9ddca16ac671ff0b76936860577882877d966ff
Frank Wang <wangjian0723@163.com>
2016-07-14 17:17:19 +0800

    add init file

---
+++ Hello-World/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  Hello-World
+//
+//  Created by PYC on 16/7/12.
+//  Copyright © 2016年 PYC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    NSLog(@"Version 001");
+    
+    NSLog(@"Version 002");
+    
+    NSLog(@"Version 003");
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
